cc16d6709c1e7ac34b6429335d73fb85c2a4e203
Hamidreza Soleimani <hamidreza.s@gmail.com>
2013-09-22 15:58:57 +0330

    first commit

---
+++ doit.rb
@@ -0,0 +1,76 @@
+require 'sinatra'
+require 'data_mapper'
+require 'json'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/doit.db")
+
+class Note
+	include DataMapper::Resource
+	property :id, Serial
+	property :content, Text, :required => true
+	property :complete, Boolean, :required => true, :default => false
+	property :created_at, DateTime
+	property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
+
+get '/' do
+	@notes = Note.all :order => :id.desc
+	@title = "All Notes"
+	completed = []
+	uncompleted = []
+	@notes.each do |item|
+		if item.complete
+			completed << item 
+		else
+			uncompleted << item
+		end
+	end
+	@notes = uncompleted.concat completed
+	erb :home
+end
+
+post '/' do
+	n = Note.new
+	n.content = params[:content]
+	n.created_at = Time.now
+	n.updated_at = Time.now
+	n.save
+	redirect '/'
+end
+
+get '/:id' do
+	@note = Note.get params[:id]
+	@title = "Edit note ##{params[:id]}"
+	erb :edit
+end
+
+put '/:id' do
+	n = Note.get params[:id]
+	n.content = params[:content]
+	n.complete = params[:complete] ? 1 : 0
+	n.updated_at = Time.now
+	n.save
+	redirect '/'
+end
+
+get '/:id/delete' do
+	@note = Note.get params[:id]
+	@title = "Confirm deletion of note ##{params[:id]}"
+	erb :delete
+end
+
+delete '/:id' do
+	n = Note.get params[:id]
+	n.destroy
+	redirect '/'
+end
+
+get '/:id/complete' do
+	n = Note.get params[:id]
+	n.complete = n.complete ? 0 : 1
+	n.updated_at = Time.now
+	n.save
+	redirect '/'
+end
