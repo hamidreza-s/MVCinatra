9b7e3fb7329ae47fb670b19885e9ee4e31fad233
Hamidreza Soleimani <hamidreza.s@gmail.com>
2013-10-26 16:47:09 +0330

    complete pagination conditions and routes

--- controllers/NoteController.rb
+++ controllers/NoteController.rb
@@ -2,25 +2,44 @@ module Controllers
   class Note < Base
 
     set_view to: self.name
-    
+
     before do
       redirect '/signin' unless session[:ui]
     end
-    
-    get '/:page?' do |page|
+
+    get '/' do
+      # default format = html
       # default page = 1
-      @notes = Model::Note.paginate page || 1,
-        user_id: session[:ui], 
-        archive: 0, 
-        order: :created_at.desc
+      # default archive = all
+      # default frequency = all
+      result = Model::Note.paginate params[:page] || 1, 
+        {
+          :conditions => 
+          {
+            :user_id => session[:ui],
+            :archive => params[:archive] || [0, 1],
+            :frequency => 
+              params[:frequency] ? 
+              params[:frequency].split(',') : 
+              ['annually', 'monthly', 'weekly','daily', 'once']
+          },
+          :order => :created_at.desc
+        }
+      @notes = result[:records]
+      @meta = result[:meta]
       @title = "All Notes"
       completed = []
       uncompleted = []
       @notes.each do |item|
         completed << item if item.complete
         uncompleted << item unless item.complete
-    end
+      end
       @notes = uncompleted.concat completed
+      # render: 
+      if params[:format] == 'json'
+        content_type :json
+        return result.to_json
+      end
       haml :home
     end
 
@@ -34,26 +53,13 @@ module Controllers
       note.updated_at = Time.now
       note.user_id = session[:ui]
       note.save
-      redirect '/'
+      # render
+      content_type :json
+      note.to_json
     end
-
-    get '/archive' do
-      @notes = Model::Note.all user_id: session[:ui], 
-        archive: 1, order: :created_at.desc 
-      @title = "Archived Notes"
-      haml :archive
-    end
-
-    get '/edit/:id' do
-      @note = Model::Note.first id: params[:id], 
-        user_id: session[:ui]
-      no_record unless @note
-      @title = "Edit note ##{params[:id]}"
-      haml :edit
-    end
-
-    put '/edit/:id' do
-      note = Model::Note.first id: params[:id], 
+    
+    put '/:id' do
+      note = Model::Note.first id: params[:id],
         user_id: session[:ui]
       no_record unless note
       note.content = params[:content]
@@ -62,36 +68,34 @@ module Controllers
       note.frequency = params[:frequency]
       note.updated_at = Time.now
       note.save
-      redirect '/'
-    end
-
-    get '/delete/:id' do
-      @note = Model::Note.first id: params[:id], 
-        user_id: session[:ui]
-      no_record unless @note
-      @title = "Confirm deletion of note ##{params[:id]}"
-      haml :delete
+      # render:
+      content_type :json
+      note.to_json
     end
 
     delete '/:id' do
-      note = Model::Note.first id: params[:id], 
+      note = Model::Note.first id: params[:id],
         user_id: session[:ui]
       no_record unless note
       note.destroy
-      redirect request.referrer
+      # render:
+      content_type :json
+      note.to_json
     end
 
     get '/archive/:id' do
-      note = Model::Note.first id: params[:id], 
+      note = Model::Note.first id: params[:id],
         user_id: session[:ui]
       no_record unless note
       note.archive = note.archive ? 0 : 1
       note.save
-      redirect request.referrer
+      # render:
+      content_type :json
+      note.to_json
     end
 
     get '/complete/:id' do
-      note = Model::Note.first id: params[:id], 
+      note = Model::Note.first id: params[:id],
         user_id: session[:ui]
       no_record unless note
       if note.complete
@@ -103,7 +107,9 @@ module Controllers
       end
       note.updated_at = Time.now
       note.save
-      redirect request.referrer
+      # render:
+      content_type :json
+      note.to_json
     end
 
   end
--- models/NoteModel.rb
+++ models/NoteModel.rb
@@ -16,9 +16,22 @@ module Model
 
     class << self
       def paginate(page, conditions)
-        note_per_page = 2
-        start_row = note_per_page * (page.to_i - 1)
-        all conditions.merge limit: note_per_page, offset: start_row
+        # meta:
+        meta = {}
+        meta[:current] = page.to_i # current page
+        meta[:rows] = 2 # rows per page
+        meta[:start] = meta[:rows] * (meta[:current] - 1) # start row
+        meta[:count] = count conditions # all record count
+        meta[:pages] = meta[:count] / meta[:rows] # all pages count
+        meta[:first] = 1 # first page
+        meta[:last] = meta[:pages] # last page
+        meta[:adjacent] = 4 # adjacent page number
+        meta[:left] = (meta[:first]..meta[:current]).to_a.last(meta[:adjacent])[0..-2].to_a
+        meta[:right] = (meta[:current]..meta[:last]).to_a.first(meta[:adjacent])[1..-1].to_a
+        # records:
+        records = all conditions.merge limit: meta[:rows], offset: meta[:start]
+        # return
+        {records: records, meta: meta}
       end
     end
 
